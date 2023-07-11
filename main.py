b83f0415b1ca1225961d2a3640d30e2003e9dff2
c4tmus <daniel.rojascarbgmail.com>
2023-07-11 17:37:23 -0500

    ejercicio ponderado

--- main.py
+++ main.py
@@ -2,11 +2,24 @@
 nombre = input('ingrese nombre')
 
 print(saludo + nombre)
-'''
-num1= int(input('ingrese numero 1:'))
 
-num2= int(input('ingrese numero 2:'))
-print (num1+num2)
-print (num1-num2)
-print (num1/num2)
-print (num1*num2)
+print (5 < 10)
+print(2>20)
+print(6<=6)
+print(0==0)
+'''
+##si prom> 13 
+	##entonces aprueba
+##sino 
+	##reprueba
+
+nota1 = int(input("nota1"))
+nota2 = int(input("nota2"))
+nota3 = int(input("nota3"))
+prom= (nota1+nota2+nota3)/3
+print("el promedio es %f"%prom)
+
+if(prom>13):	
+ 	print("aprueba")
+else:
+ 	print("desaprueba")
