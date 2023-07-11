38a5c53840abbd38d02d2cdb2d39c9319f039ce0
c4tmus <daniel.rojascarbgmail.com>
2023-07-11 17:21:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''saludo =  "hola"
+nombre = input('ingrese nombre')
+
+print(saludo + nombre)
+'''
+num1= int(input('ingrese numero 1:'))
+
+num2= int(input('ingrese numero 2:'))
+print (num1+num2)
+print (num1-num2)
+print (num1/num2)
+print (num1*num2)
